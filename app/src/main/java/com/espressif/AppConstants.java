0740be68ff05bcc47975b65def42345726808d88
khushbhushah <khushbu.shah@espressif.com>
2019-06-12 13:56:24 +0530

    Completed provision for wifi access point with open (no security).

--- app/src/main/java/com/espressif/AppConstants.java
+++ app/src/main/java/com/espressif/AppConstants.java
@@ -9,4 +9,8 @@ public class AppConstants {
     public static final short WIFI_WPA2_PSK = 3;
     public static final short WIFI_WPA_WPA2_PSK = 4;
     public static final short WIFI_WPA2_ENTERPRISE = 5;
+
+    // Keys used to pass data between activities and to store data in SharedPreference.
+    public static final String KEY_PROOF_OF_POSSESSION = "proof_of_possession";
+    public static final String KEY_WIFI_SECURITY_TYPE = "wifi_security";
 }
