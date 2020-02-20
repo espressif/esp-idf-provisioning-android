9da5d0cf47bfbf1e5bb88d966aaca684febc277f
khushbhushah <khushbu.shah@espressif.com>
2020-02-20 11:02:06 +0530

    Migrated to AndroidX.

--- app/src/main/java/com/espressif/ui/activities/AppInfoActivity.java
+++ app/src/main/java/com/espressif/ui/activities/AppInfoActivity.java
@@ -3,9 +3,9 @@ package com.espressif.ui.activities;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.widget.TextView;
 
 import com.espressif.provision.R;
--- app/src/main/java/com/espressif/ui/activities/DeviceInfoActivity.java
+++ app/src/main/java/com/espressif/ui/activities/DeviceInfoActivity.java
@@ -2,9 +2,9 @@ package com.espressif.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.widget.TextView;
 
 import com.espressif.AppConstants;
