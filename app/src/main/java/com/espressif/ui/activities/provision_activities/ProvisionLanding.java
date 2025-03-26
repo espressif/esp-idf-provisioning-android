09854a1b3128660ed72aeb0a2b0b620fe92a6668
WenerrP <202010282@itla.edu.do>
2025-03-26 17:25:45 -0400

    Un poco de reestructuracion del codigo

--- app/src/main/java/com/espressif/ui/activities/provision_activities/ProvisionLanding.java
+++ app/src/main/java/com/espressif/ui/activities/provision_activities/ProvisionLanding.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.espressif.ui.activities;
+package com.espressif.ui.activities.provision_activities;
 
 import android.Manifest;
 import android.content.Intent;
